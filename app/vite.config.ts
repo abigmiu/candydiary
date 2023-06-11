ed0474c02cf5f2f10a8a945a2558f8ae6a853746
abigmiu <suraelax@gmail.com>
2023-06-11 16:15:59 +0800

    chore: scss additionalData

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import UnoCSS from 'unocss/vite';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,9 +17,19 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import '@/assets/styles/_variable.scss';`,
-            
+                // additionalData: `@import '@/assets/styles/_variable.scss';`,
+                additionalData: (content: string, filename: string) => {
+                    let result = content;
+                    // 设置页面引入setting.scss
+                    const settingPages = path.resolve(__dirname, 'src/Setting');
+                    if (filename.startsWith(settingPages)) {
+                        result = `@import '@/assets/styles/setting.scss';${result}`
+                    }
+                    result = `@import '@/assets/styles/_variable.scss';${result}`
+                    return result;
+                }
             }
+                
         }
     }
 });
